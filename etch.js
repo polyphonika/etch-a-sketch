99e9d154e2573699fad47fa17e9a41784fcef542
Ryan Berry <email@cornerhousemedia.com>
2024-10-01 18:13:54 +0100

    first drawing works

--- etch.js
+++ etch.js
@@ -7,3 +7,22 @@ for (let i=0; i<(16*16); i++) {
     pixel.className = 'pixel';
     container.appendChild(pixel);
 }
+
+// Handle Drawing
+
+const pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach(element => {
+    element.addEventListener('mouseenter', function(e) {
+        addPixelColour(e)
+    })
+});
+
+function addPixelColour(e) {
+    // e.target.style.background = 'black';
+    // e.target.className = 'pixel drawn'
+    e.target.classList.add('drawn');
+    
+}
+
+
